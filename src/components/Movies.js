35d236e85d0550bb20dfc3162c9f8b7bffa211c3
tatertot365 <tate.gillespie@gmail.com>
2023-03-27 18:05:41 -0600

    first commit

---
+++ src/components/Movies.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import MovieData from './MovieData.json';
+
+const Movies = () => {
+    return (
+        <div className="container">
+            <h1>My Movies</h1>
+            <p>This is where we will list all of Joel's movies.</p>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Year</th>
+                        <th>Rating</th>
+                        <th>Genre</th>
+                        <th>Director</th>
+                        <th>Edited</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {MovieData.MovieData.map((movie) => (
+                        <tr key={movie.Title}>
+                            <td>{movie.Title}</td>
+                            <td>{movie.Year}</td>
+                            <td>{movie.Rating}</td>
+                            <td>{movie.Category}</td>
+                            <td>{movie.Director}</td>
+                            <td>{movie.Edited}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Movies;
---
+++ src/components/Podcasts.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Podcasts = () => {
+    return (
+        <div className="container">
+            <h1>My Podcasts</h1>
+            <p>
+                Check out Joel's podcast website at {'https://baconsale.com/'}
+            </p>
+            <a href="https://baconsale.com/" className="btn btn-primary">
+                Go to BaconSale
+            </a>
+        </div>
+    );
+};
+
+export default Podcasts;
